a378cda35552f79258dc2ebd6da1acd884e241c3
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2021-12-06 13:06:37 +0200

    popitka nomer 5

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ $('.menu-mobile__btn').click(function () {
   $(this).toggleClass('is-active');
   $('.menu-mobile__list').toggleClass('is-open');
 });
+
+$('.menu-mobile__link').click(function () {
+  $('.menu-mobile__list').toggleClass('is-open');
+  $('.menu-mobile__btn').toggleClass('is-active');
+});
