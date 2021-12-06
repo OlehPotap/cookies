8a7372c3b9a27ab3ab58246aa5530c2a3444b755
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2021-12-06 13:37:07 +0200

    подписал javascript

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ $('.menu-mobile__btn').click(function () {
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-open]'),
-    closeModalBtn: document.querySelector('[data-close]'),
-    modal: document.querySelector('[paragraph-show]'),
+    readmorebtn: document.querySelector('[data-open]'),
+    lessbtn: document.querySelector('[data-close]'),
+    paragraph: document.querySelector('[paragraph-show]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.readmorebtn.addEventListener('click', toggleModal);
+  refs.lessbtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('about-us_is-hidden');
-    refs.openModalBtn.classList.toggle('button__is-hidden');
-    refs.closeModalBtn.classList.toggle('button__is-hidden');}
+    refs.paragraph.classList.toggle('about-us_is-hidden');
+    refs.readmorebtn.classList.toggle('button__is-hidden');
+    refs.lessbtn.classList.toggle('button__is-hidden');}
     
 })();
