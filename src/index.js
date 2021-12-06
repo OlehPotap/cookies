bde10c50d6035c62ca6a2446208c6c17901d9bac
OlehPotap <potap.oleh.v@gmail.com>
2021-12-06 13:30:54 +0200

    fdgds

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ $('.menu-mobile__btn').click(function () {
   $(this).toggleClass('is-active');
   $('.menu-mobile__list').toggleClass('is-open');
 });
+
+$('.menu-mobile__link').click(function () {
+  $('.menu-mobile__list').toggleClass('is-open');
+  $('.menu-mobile__btn').toggleClass('is-active');
+});
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-open]'),
+    closeModalBtn: document.querySelector('[data-close]'),
+    modal: document.querySelector('[paragraph-show]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('about-us_is-hidden');
+    refs.openModalBtn.classList.toggle('button__is-hidden');
+    refs.closeModalBtn.classList.toggle('button__is-hidden');}
+})();
