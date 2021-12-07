8324a1359061191187113a5ae9bae86f82bbb1cb
WebStudioFlip <73443296+WebStudioFlip@users.noreply.github.com>
2021-12-07 16:14:27 +0200

    update

--- src/js/slider.js
+++ src/js/slider.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
     centerMode: true,
     asNavFor: '.reviews__bottom',
     centerPadding: '0px',
+    focusOnSelect: true,
     responsive: [
       {
         breakpoint: 767,
@@ -19,9 +20,25 @@ $(document).ready(function () {
   $('.reviews__bottom').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
+    dots: true,
     arrows: false,
     centerMode: true,
     centerPadding: '0px',
     asNavFor: '.reviews__list',
+    customPaging: function(slider, i) {
+      var current = i + 1;
+      current = current < 10 ? "0" + current : current;
+  
+      var total = slider.slideCount;
+      total = total < 10 ? "0" + total : total;
+  
+      return (
+        '<button type="button" role="button" tabindex="0" class="slick-dots-button">\
+        <span class="slick-dots-current">' + current + '</span>\
+        <span class="slick-dots-separator">/</span>\
+        <span class="slick-dots-total">' + total + '</span>\
+      </button>'
+      );
+    }
   });
 });
